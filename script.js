c77bc65348782a6a3d46340c3f1df3956a30c55d
thiagofuertes <132574600+thiagofuertes@users.noreply.github.com>
2023-07-03 02:21:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+//function saludar(){
+    //console.log("hola hijo de puta");
+   //alert("hola hijo de puta")
+//}
+function fondo() {
+document.body.style.background = "red";
+}
+function fondo2() {
+    document.body.style.background = "white";
+    }
+    function fondo3() {
+        document.body.style.background = "yellow";
+        }
+    /*alert(location.href); 
+    if (confirm("Ir a wikipedia?")) {
+        location.href = "https://wikipedia.org";
+        } */
